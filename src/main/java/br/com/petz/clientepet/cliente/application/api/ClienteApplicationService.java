305359968f89427c6584c20184a0a2b5ce44a37b
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-02-02 06:14:52 -0300

    implementa ClienteApplicationService e corrige ClienteAPI

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteApplicationService.java
@@ -0,0 +1,19 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class ClienteApplicationService implements ClienteService {
+
+	@Override
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteApplicationService - criaCliente");
+		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
+		log.info("[finaliza] ClienteApplicationService - criaCliente");
+		return null;
+	}
+
+}
