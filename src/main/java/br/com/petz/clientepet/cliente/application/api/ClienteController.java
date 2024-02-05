1e7ef6c2386a67a246bde7e37081dd2b2cd68535
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-02-05 05:07:41 -0300

    implementa ClienteApplicationService e novo Construtor Cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.petz.clientepet.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
