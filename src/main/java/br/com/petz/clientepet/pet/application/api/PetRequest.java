d3a37cfba385649c3fa87e58a072087acced480d
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-02-17 23:20:26 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetRequest.java
@@ -1,5 +1,35 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.petz.clientepet.pet.domain.Porte;
+import br.com.petz.clientepet.pet.domain.SexoPet;
+import br.com.petz.clientepet.pet.domain.tipoPet;
+import lombok.Value;
+
+@Value
 public class PetRequest {
+	@NotBlank
+	private String nomePet;
+	private Porte porte;
+	@NotNull
+	private tipoPet tipo;
+	private String microchip;
+	@NotBlank
+	private String raca;
+	@NotNull
+	private SexoPet sexo;
+	private String pelagemCor;
+	@NotNull
+	private LocalDate dataNascimento;
+	private String rga;
+	private Integer peso;
 
 }
--- src/main/java/br/com/petz/clientepet/pet/application/api/PetResponse.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetResponse.java
@@ -1,5 +1,8 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import java.util.UUID;
+
 public class PetResponse {
+	private UUID idPet;
 
 }
